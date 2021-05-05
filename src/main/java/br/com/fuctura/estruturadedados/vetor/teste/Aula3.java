d190ad5be29a5971702f08f6e3a97ccb81263563
Augusto <negocios.augusto@gmail.com>
2021-05-05 12:58:00 -0300

    Início do curso de estrutura de dados

---
+++ src/main/java/br/com/fuctura/estruturadedados/vetor/teste/Aula3.java
@@ -0,0 +1,15 @@
+package br.com.fuctura.estruturadedados.vetor.teste;
+
+import br.com.fuctura.estruturadedados.vetor.Vetor;
+
+
+public class Aula3 {
+
+    public static void main(String[] args) {
+
+        Vetor vetor = new Vetor(5);
+        vetor.adiciona("Elemento 1");
+        vetor.adiciona("Elemento 2");
+    }
+
+}
