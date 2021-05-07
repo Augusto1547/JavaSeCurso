a27161e80c78a5c4cafa4865699187e1c4bf7add
Augusto <negocios.augusto@gmail.com>
2021-05-07 08:24:23 -0300

    estrutura de dados: adicionar elemento no fim do vetor

--- src/main/java/br/com/fuctura/estruturadedados/vetor/Vetor.java
+++ src/main/java/br/com/fuctura/estruturadedados/vetor/Vetor.java
@@ -1,5 +1,5 @@
 package br.com.fuctura.estruturadedados.vetor;
-
+//Title: Adicionar elemento no final do vetor
 public class Vetor {
     //Valor inicial de:  String = null, 
     //boolean: false, int,char,float, etc = 0, 
@@ -15,25 +15,41 @@ public class Vetor {
 
     /*public void adiciona(String elemento) {
 
-        for (int i = 0; i < this.elementos.length; i++) {
-
-            if (this.elementos[i] == null) {
-
+        //iterar para adicionar elemento
+        
+        for(int i = 0 ; i < this.elementos.length; i++){
+            
+            if(this.elementos[i] == null){
+                
                 this.elementos[i] = elemento;
-                break;//interrompe assim que encontrar o primeiro elemenot null
+                break;
+                
             }
         }
-    }*/
-    public void adiciona(String elemento) throws Exception {
+    }*/ //Essa função adiciona dois elmentos se encontrar um nulo
+    /*public void adiciona(String elemento) throws Exception {
 
         if (this.tamanho < this.elementos.length) {
 
             this.elementos[this.tamanho] = elemento;
             this.tamanho++;
+
         } else {
 
-            throw new Exception("Vetor já está cheio não é possível adicionar mais elementos");
+            throw new Exception("Vetor está cheio não é possível adicionar mais elementos");
+        }
+
+    }*/
+    public boolean adiciona(String elemento) {
+
+        if (this.tamanho < this.elementos.length) {
+
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
+            return true;
 
         }
+        return false;
     }
+
 }
---
+++ src/main/java/br/com/fuctura/estruturadedados/vetor/testaVetor.java
@@ -0,0 +1,30 @@
+package br.com.fuctura.estruturadedados.vetor;
+
+public class testaVetor {
+
+    public static void main(String[] args) {
+
+        /*Vetor vetor = new Vetor(5);
+        vetor.adiciona("Elemento 1");
+        vetor.adiciona("Elemento 2");*/
+       
+        /*Vetor vetor = new Vetor(2); //tem capacidade apenas para dois
+        try {
+
+            vetor.adiciona("Elemento 1");
+            vetor.adiciona("Elemento 2");
+            vetor.adiciona("Elemento 3");
+
+        } catch (Exception ex) {
+
+            ex.printStackTrace();
+        }*/
+        
+        Vetor vetor = new Vetor(2);
+        vetor.adiciona("Elemento 1");
+        vetor.adiciona("Elemento 2");
+        vetor.adiciona("Elemento 3");
+
+    }
+
+}
