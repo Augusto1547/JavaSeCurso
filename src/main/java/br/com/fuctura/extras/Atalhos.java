11b661a850f61997044603d196797448dfa994a1
Augusto <negocios.augusto@gmail.com>
2021-05-23 11:32:30 -0300

    tipos de atalhos eclipse aula de Domingo

--- src/main/java/br/com/fuctura/extras/Atalhos.java
+++ src/main/java/br/com/fuctura/extras/Atalhos.java
@@ -1,37 +1,43 @@
 package br.com.fuctura.extras;
 
 public class Atalhos {
+//no google: eclipse color theme
+
+	String cidade;
+	int idade;
 
 	public static void main(String[] args) {
-		
+
 		String nome = "Rodrigo";
-		String nome2 = "pedro";
+
+		// @formatter:off
+		       String nome2 = "pedro";
+		// @formatter:on
 
 		System.out.println("Rodrigo");
-		
 		/*
-		 * Ir para Preferences> Java> Editor> Content Assiste escrever .abcdefghijklmnopqrstuvwxyzno Auto activation triggers for Javacampo.
-		 * (para ativar as opções ampliadas de código)
-		 * CTRL + Shift + O (importa o pacote)
-		 * contrl + shift + f (identa o código)
-		 * control + 3 (procura recursos)
-		 * control + e (navega entre as classes criadas)
-		 * control + alt + pagedown (também navega entre as classes)
-		 * Alt + Shift + C (muda a assinatura do método);
-		 * Alt + shifit + A (seleciona e muda o nome de todos)
-		 * Alt + shift + r (renomeia a variável)
-		 * Control + Shift + x/y ( transforma o trecho selecionado em maiúscula ou menuscula)
-		 * Control + Shift + g (ver uma referência mais detalhada da classe, variável etc)
-		 * Control + shift [ (Divide a mesma classe em duas janelas lado a lado)
-		 * Control + 1 (janela para excluir funções variáveis e outros recursos)
-		 * control + d (exclui linha)
-		 * control + m (janela lateral)
-		 * control + f11 (exibir a saída do programa)
-		 * alt + f11 (expandir o tamnho da janela)
-		 * windows > showviews > templates (criar um alias)
-		 * */	
-		
-				
-	}
+		 * Ir para Preferences> Java> Editor> Content Assiste escrever .
+		 * abcdefghijklmnopqrstuvwxyzno Auto activation triggers for Javacampo. (para
+		 * ativar as opções ampliadas de código) CTRL + Shift + O (importa o pacote)
+		 * contrl + shift + f (identa o código) control + 3 (procura recursos) control +
+		 * e (navega entre as classes criadas) control + alt + pagedown (também navega
+		 * entre as classes) Alt + Shift + C (muda a assinatura do método); Alt + shifit
+		 * + A (seleciona e muda o nome de todos) Alt + shift + r (renomeia a variável)
+		 * Control + Shift + x/y ( transforma o trecho selecionado em maiúscula ou
+		 * menuscula) Control + Shift + g (ver uma referência mais detalhada da classe,
+		 * variável etc) Control + shift [ (Divide a mesma classe em duas janelas lado a
+		 * lado) Control + 1 (sugere alguma alternativa para a solução de um erro)
+		 * control + 3 (ggas(gets e sets), ghcae(iquals e rache code), gcuf (construtores)) 
+		 * control + d (deleta uma linha)
+		 * control + m (janela lateral) control + f11 (exibir a saída do
+		 * control + / (adiciona ou remove um comentário de linha única)
+		 * control + shift + seta (início e fim do documento)
+		 * programa) alt + f11 (expandir o tamnho da janela) windows > showviews >
+		 * templates (criar um alias)
+		 * alt + seta(cima,baixo) (vomer o trecho pela tela)
+		 */
 
+	}
+ 
+	
 }
---
+++ src/main/java/br/com/fuctura/extras/TestaAtalho.java
@@ -0,0 +1,13 @@
+package br.com.fuctura.extras;
+
+public class TestaAtalho {
+	
+	
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
