45d77ea7bc5d9945dd421c7f636a8e214f25d050
Augusto César <negocios.augusto@gmail.com>
2021-05-26 12:19:37 -0300

    adicionado mais atalhos

--- src/main/java/br/com/fuctura/extras/Atalhos.java
+++ src/main/java/br/com/fuctura/extras/Atalhos.java
@@ -15,30 +15,51 @@ public class Atalhos {
 		// @formatter:off
 		       String nome2 = "pedro";
 		// @formatter:on
+		       
+		       mostrarNome(nome);
 
 		System.out.println("Rodrigo");
 		/*
-		 * Ir para Preferences> Java> Editor> Content Assiste escrever .
-		 * abcdefghijklmnopqrstuvwxyzno Auto activation triggers for Javacampo. (para
-		 * ativar as opções ampliadas de código) CTRL + Shift + O (importa o pacote)
-		 * contrl + shift + f (identa o código) control + 3 (procura recursos) control +
-		 * e (navega entre as classes criadas) control + alt + pagedown (também navega
-		 * entre as classes) Alt + Shift + C (muda a assinatura do método); Alt + shifit
-		 * + A (seleciona e muda o nome de todos) Alt + shift + r (renomeia a variável)
+		 * windows > preferences > geral > key (para criar seu próprio atalho)
+		 * ativar as opções ampliadas de código) 
+		 * CTRL + Shift + O (importa o pacote)
+		 * contrl + shift + f (identa o código) 
+		 * control + e (navega entre as classes criadas) 
 		 * Control + Shift + x/y ( transforma o trecho selecionado em maiúscula ou
-		 * menuscula) Control + Shift + g (ver uma referência mais detalhada da classe,
-		 * variável etc) Control + shift [ (Divide a mesma classe em duas janelas lado a
-		 * lado) Control + 1 (sugere alguma alternativa para a solução de um erro)
+		 * control + f (localizar e substituir)
+		 * control + alt + pagedown (também navega
+		 * Control + shift [ (Divide a mesma classe em duas janelas lado a
+		 * lado) 
+		 * Control + 1 (sugere alguma alternativa para a solução de um erro)
 		 * control + 3 (ggas(gets e sets), ghcae(iquals e rache code), gcuf (construtores)) 
 		 * control + d (deleta uma linha)
 		 * control + m (janela lateral) control + f11 (exibir a saída do
 		 * control + / (adiciona ou remove um comentário de linha única)
 		 * control + shift + seta (início e fim do documento)
+		 * entre as classes) 
+		 * Control + Shift + g (ver uma referência mais detalhada da classe,
+		 * variável etc) 
+		 * Control + shift + l (listar todos os atalhos do eclipse)
+		 * control + t (em cima de uma classe para ver quem é o pai daquela classe)
+		 * Alt + Shift + C (muda a assinatura do método);
+		 *  Alt + shifit + A (seleciona e muda o nome de todos) 
+		 *  Alt + shift + r (renomeia a variável em todos os lugares)
 		 * programa) alt + f11 (expandir o tamnho da janela) windows > showviews >
 		 * templates (criar um alias)
 		 * alt + seta(cima,baixo) (vomer o trecho pela tela)
 		 * alt + shifit + r (mudei o nome da variável e automaticamente foi alterado em outras partes do código também)
+		 * alt + shift + m (seleciona o if e da o comando para criar uma função e colocar o if dentro)
 		 */
+
+	}
+
+	public static void mostrarNome(String nome) {
+		if(nome == "rodrigo") {
+			   
+			   System.out.println("olá rodriro");
+		   }
+	}
+ 
 
 	}
  
